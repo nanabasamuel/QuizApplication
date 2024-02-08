02041b36dbc4fbe291ac325c5cb29ae5bae89743
nanabasamuel <ankomah2g5@gmail.com>
2024-02-08 11:29:05 +0000

    Almost Commit

--- src/main/java/com/example/quizapplication/QuizResults.java
+++ src/main/java/com/example/quizapplication/QuizResults.java
@@ -1,8 +1,14 @@
 package com.example.quizapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.window.OnBackInvokedDispatcher;
 
 public class QuizResults extends AppCompatActivity {
 
@@ -10,5 +16,32 @@ public class QuizResults extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_results);
+
+        final AppCompatButton startNewBtn = findViewById(R.id.startNewQuizBtn);
+        final TextView correctAnswer = findViewById(R.id.correctAnswers);
+        final TextView incorrectAnswers = findViewById(R.id.incorrectAnswers);
+
+        final int getCorrectAnswers = getIntent().getIntExtra("correct", 0);
+        final int getIncorrectAnswers = getIntent().getIntExtra("incorrect", 0);
+
+        correctAnswer.setText(String.valueOf(getCorrectAnswers));
+        incorrectAnswers.setText(String.valueOf(getIncorrectAnswers));
+
+        startNewBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(QuizResults.this, MainActivity.class));
+                finish();
+            }
+        });
+    }
+
+    @NonNull
+    @Override
+    public OnBackInvokedDispatcher getOnBackInvokedDispatcher() {
+
+        startActivity(new Intent(QuizResults.this, MainActivity.class));
+        finish();
+        return null;
     }
 }
